95eda660e62b24479a88ef6c15e50e67d397844e
Bandi Kanti Chaitanya <bandikantichaitanya2@gmail.com>
2025-04-25 18:09:08 +0530

    updated version

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,18 @@ import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = (props) => {
-
+  let [content,setContent]=useState(false)
+function showContent(){
+  setContent(true)
+}
   return (
     <div className="App" id="main">
-      // Do not alter the main div
+      {content ?
+     <p id="para" className="show" >Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy.</p>
+     :
+     <p  id="para" className="hide"></p>
+      }
+     <button id="click" onClick={showContent}  >Show</button>
     </div>
   );
 }
